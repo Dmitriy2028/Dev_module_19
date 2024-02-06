c3edd4447df4fec8f9d1251ad5a290dfcd8e95dc
Dmitriy <dmitriy.ilin.98@gmail.com>
2024-02-06 17:46:22 +0400

    Initial commit

---
+++ src/main/java/org/example/SumCalculator.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class SumCalculator {
+    int sum(int n){
+        if (n <= 0) {
+            throw new IllegalArgumentException("n має бути додатнім числом");
+        }
+        int result = 0;
+        for (int i = 1; i <= n; i++) {
+            result += i;
+        }
+        return result;
+    }
+}
